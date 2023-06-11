11c446a1305a6c3b63cabf9332f418f860d48dc4
Yusuf Fazeri <yusuffazeri00@gmail.com>
2023-06-11 19:04:56 +0700

    adding component test for list posts

---
+++ src/app/ListPosts.cy.js
@@ -0,0 +1,18 @@
+import ListPosts from "./ListPosts";
+
+describe("ListPosts", () => {
+  beforeEach(() => {
+    cy.intercept("GET", "http://localhost:3030/posts*", {
+      fixture: "posts.json",
+    }).as("getPosts");
+    cy.mount(<ListPosts />);
+    cy.wait("@getPosts");
+  });
+
+  it("should render the list of posts", () => {
+    cy.get("[data-testid='list-posts'] li").should(
+      "have.length.greaterThan",
+      0
+    );
+  });
+});
